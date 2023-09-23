7e031f288adcadf27905a46f1f4cc44149af1189
Huy Tran <quanghuy.hut.fami@gmail.com>
2023-09-23 16:08:19 +0200

    Move setup file to root folder

---
+++ local_startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Add check database is up and running
+if [ ! "$(docker ps -q -f name=db)" ]; then
+    # run your container
+    docker run -d --name db -e POSTGRES_USER=admin \
+      -e POSTGRES_PASSWORD=admin -e POSTGRES_DB=timeregister \
+      -p 5432:5432 postgres:13.1-alpine
+fi
+
+export SPRING_DATASOURCE_URL="jdbc:postgresql://localhost:5432/timeregister"
+export DATABASE_URL_CUSTOMIZE="jdbc:postgresql://localhost:5432/timeregister"
+export USER_NAME=admin
+export USER_PASSWORD=admin
+export JWT_SECRET_KEY=cbfZjNawhGCMkqA7yuQyMT7XmepLvW1T123avd
+./gradlew bootRun
+
+
